caacf450033709f9f2a332a0fa042ef1b76681a7
Bikrant <bikrantjung1214@gmail.com>
2023-12-19 23:01:13 +0545

    Home dashboard page

--- src/app/app/(dashboard)/layout.tsx
+++ src/app/app/(dashboard)/layout.tsx
@@ -5,12 +5,12 @@ import { ReactNode, Suspense } from "react";
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <div>
-      <Nav>
+      {/* <Nav>
         <Suspense fallback={<div>Loading...</div>}>
           <Profile />
         </Suspense>
-      </Nav>
-      <div className="min-h-screen dark:bg-black sm:pl-60">{children}</div>
+      </Nav> */}
+      <div className="min-h-screen p-12 dark:bg-black">{children}</div>
     </div>
   );
 }
